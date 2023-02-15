ecf08ddf58787ca2a1ba106bafd02003724c8739
uday chauhan <chauhanuday842@gmail.com>
2023-02-15 15:31:56 +0530

    first commit

---
+++ src/componets/assign01/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+   <>
+    <h1 style={{color:"black"}}>About us</h1>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, voluptatem?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis quisquam pariatur sequi reprehenderit odio reiciendis earum, tempore porro rerum laborum. Officia nihil soluta quasi laborum dolores cumque numquam magni, omnis ipsam provident aut nesciunt earum fugit. Accusantium voluptate ducimus placeat quas officiis fuga excepturi, voluptates explicabo sequi, nihil expedita libero.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi a corrupti eos officiis neque sapiente, molestias esse ipsa iure id._________</p>
+   </>
+  )
+}
+
+export default Home;
---
+++ src/componets/assign02/Home1.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './home.css'
+const Home1 = () => {
+  return (
+    <>
+
+      <div className='divparent'>
+        <div className='divheading'>
+          <h1>the evolution of airbnb's forntend</h1>
+        </div>
+        <div className='divdown'>
+          <p>@spikebrehm</p>
+          <figure>
+          <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ4oacEumRwwIXfWmFjhGAn7rLrgKwg-5kVmTgmbgi37YlDprxo" alt="" />
+          <figcaption>airbnb</figcaption>
+          </figure>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Home1;
